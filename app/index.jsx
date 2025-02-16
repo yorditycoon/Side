4f8942c84bd97245112ede9f8c36a8b765193d9e
Yonas19 <yonjr0936@gmail.com>
2025-02-16 16:54:08 +0400

    hello

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,17 @@
-<<<<<<< HEAD
 import { StyleSheet, Text, View, Image } from "react-native";
-=======
 import { StyleSheet, Text, View } from "react-native";
->>>>>>> 93b76e9 (file changed)
 
 export default function Page() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
       <Image
         source={require("../assets/images/side-logo.png")}
         style={{ width: 400, height: 200 }}
       />
-=======
       <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
+        <Text style={styles.title}>Hello Yordanos</Text>
         
       </View>
->>>>>>> 93b76e9 (file changed)
     </View>
   );
 }
@@ -26,20 +20,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-<<<<<<< HEAD
     justifyContent: "center",
-=======
->>>>>>> 93b76e9 (file changed)
     padding: 24,
   },
   main: {
     flex: 1,
     justifyContent: "center",
-<<<<<<< HEAD
 
-=======
     maxWidth: 960,
->>>>>>> 93b76e9 (file changed)
     marginHorizontal: "auto",
   },
   title: {
@@ -47,13 +35,10 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   subtitle: {
-<<<<<<< HEAD
     fontSize: 44,
     color: "#38434D",
     fontWeight: 500,
-=======
     fontSize: 36,
     color: "#38434D",
->>>>>>> 93b76e9 (file changed) 
   },
 });
